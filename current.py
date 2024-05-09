a6814cf88795255558cf9b8b3a9792955be637a8
Samyak Verma <samyak.verma@utah.edu>
2024-05-09 15:40:41 -0600

    proc splitting

--- current.py
+++ current.py
@@ -24,49 +24,54 @@ def record_data_in_excel(current, file_name):
 	data.to_excel(file_name)
 	print(f"Data recorded in {file_name}")
 
-def collect_data(machines_dict, milliseconds_between_measurements):
-	
-
-def main():
-	TOMLSettingsHolder = TOMLSettings()
-	if TOMLSettingsHolder.using_toml() is False:
+def check_and_get_filename(SettingsHolder: TOMLSettings):
+	if SettingsHolder.using_toml() is False:
 		print("You must set USE_TOML to true in MagneticFieldSettings.toml to use this script.")
 		return
 	
-	toml_file_name = TOMLSettingsHolder.get("file_name")
-	if(toml_file_name == TOMLSettingsHolder.defaults["file_name"]):
+	toml_file_name = SettingsHolder.get("file_name")
+	if(toml_file_name == SettingsHolder.defaults["file_name"]):
 		toml_file_name = f"output-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.xlsx"
 		print(f"We are loading a default file name from TOML, using a safety feature to avoid this. Will save as {toml_file_name}")
 
-	file_name = f"output/{toml_file_name}"
+def setup_machines(SettingsHolder: TOMLSettings):
 	rm = visa.ResourceManager()
 	try:
 		KEPCO = PowerSupply(
 			'GPIB0::6::INSTR',
 			rm,
-			voltage = TOMLSettingsHolder.get("powersupply_voltage_compliance"),
-			current = TOMLSettingsHolder.get("powersupply_current"),
+			voltage = SettingsHolder.get("powersupply_voltage_compliance"),
+			current = SettingsHolder.get("powersupply_current"),
 		)
 		Nanovoltmeter = VoltageNanovoltmeter(
 			'GPIB0::8::INSTR',
 			rm,
-			voltmeter_range = TOMLSettingsHolder.get("nanovoltmeter_range"),
+			voltmeter_range = SettingsHolder.get("nanovoltmeter_range"),
 		)
 		KeithleySource = CurrentSource(
 			'GPIB0::10::INSTR',
 			rm,
-			amperage = TOMLSettingsHolder.get("currentsource_amperage"),
-			compliance = TOMLSettingsHolder.get("currentsource_compliance"),
+			amperage = SettingsHolder.get("currentsource_amperage"),
+			compliance = SettingsHolder.get("currentsource_compliance"),
 		)
 	except visa.VisaIOError as e:
 		print(f"Could not connect to one of the machines. Please check your connections. {e}")
 		return
 	
-	machines_dict = {
+	return {
 		"Power Supply": KEPCO,
 		"Voltage Nanovoltmeter": Nanovoltmeter,
 		"Current Source": KeithleySource,
 	}
+
+def collect_data(machines_dict, milliseconds_between_measurements):
+
+
+def main():
+	TOMLSettingsHolder = TOMLSettings()
+
+	file_name = check_and_get_filename(TOMLSettingsHolder)
+	machines_dict = setup_machines(TOMLSettingsHolder)
 
 	results = collect_data(machines_dict, TOMLSettingsHolder.get("milliseconds_between_measurements"))
 
